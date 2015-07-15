7d2ba9931b161e5cf4d6c4b843883043dc35c6b4
Michelle Z <mzulli@users.noreply.github.com>
2015-07-15 06:42:57 -0400

    Update a_list.js

--- a_list.js
+++ a_list.js
@@ -3,3 +3,56 @@
 If you haven’t already, also write a recursive version of nth.
 */
 
+function arrayToList(array) {
+  var list = {};
+  var tempList = {};
+  for (var i = array.length - 1; i >= 0; i--) {
+    if (i === array.length - 1) {
+      list = {value: array[i], rest: null};
+      tempList = list;
+    }
+    else {
+     list = {value: array[i], rest: tempList};
+     tempList = list;
+    }
+  }
+  return list;
+}
+
+function listToArray(list) {
+  var arr = [];
+  for (var node = list; node; node = node.rest) {
+    arr.push(node.value);
+  }
+  return arr;
+}
+
+function prepend(element, list) {
+  var newList = {};
+  newList = {value: element, rest: list};
+  return newList;
+}
+
+function nth(list, number) {
+  var arr = listToArray(list);
+  return arr[number];
+}
+
+function nthR(list, number) {
+  while (list.rest !== null) {
+    list = list.rest;
+    number = number - 1;
+  }
+  if (number === 0) return list.value;
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
+console.log(nthR(arrayToList([10, 20, 30]), 1));
+// → 20
