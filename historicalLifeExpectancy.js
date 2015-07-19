6381de1f4d1a78d2ec99c18f219c6296967b28e9
Michelle Z <mzulli@users.noreply.github.com>
2015-07-19 06:24:45 -0400

    Update historicalLifeExpectancy.js

--- historicalLifeExpectancy.js
+++ historicalLifeExpectancy.js
@@ -1,3 +1,11 @@
+/* Historical life expectancy */
+
+/* 
+When we looked up all the people in our data set that lived more than 90 years, only the latest generation in the data came out. Let’s take a closer look at that phenomenon.
+
+Compute and output the average age of the people in the ancestry data set per century. A person is assigned to a century by taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100).
+*/
+
 function averageOneDec(array) {
   function plus(a, b) { return a + b; }
   return Math.round(array.reduce(plus) / array.length * 10)/10;
